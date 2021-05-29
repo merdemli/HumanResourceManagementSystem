54b476118d4fb6a156584316f9cdff863c0f97de
merdemli <57098477+merdemli@users.noreply.github.com>
2021-05-29 22:17:32 +0300

    Core Utilities Results Layer Added

--- src/main/java/io/hrms/core/utilities/results/Result.java
+++ src/main/java/io/hrms/core/utilities/results/Result.java
@@ -3,5 +3,23 @@ package io.hrms.core.utilities.results;
 public class Result {
 	private boolean success;
 	private String message;
+
+	public Result(boolean success) {
+		this.success = success;
+	}
+
+	public Result(boolean success, String message) {
+		
+		this(success);                    //this.success = success;
+		this.message = message;
+		
+	}
 	
+	public boolean isSuccess() { //getSuccess
+		return this.success;
+	} 
+
+	public String getMassage() {
+		return this.message; 
+	}
 }
