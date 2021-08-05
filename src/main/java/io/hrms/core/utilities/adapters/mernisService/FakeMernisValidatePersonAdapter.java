c02a696a86298f6474b6843b6f522cc7254a92cb
merdemli <57098477+merdemli@users.noreply.github.com>
2021-08-06 02:36:24 +0300

    FakeMernisServis Added

---
+++ src/main/java/io/hrms/core/utilities/adapters/mernisService/FakeMernisValidatePersonAdapter.java
@@ -0,0 +1,26 @@
+package io.hrms.core.utilities.adapters.mernisService;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import io.hrms.entities.concretes.JobSeeker;
+import io.hrms.fakeMernisService.FakeMernisValidationService;
+
+@Component
+@Primary
+public class FakeMernisValidatePersonAdapter implements ValidatePersonService{
+
+	@Override
+	public boolean CheckIfRealPerson(JobSeeker jobSeeker ){
+
+		FakeMernisValidationService client = new FakeMernisValidationService();
+	
+           boolean result= client.validatePerson(jobSeeker);
+           result = true;
+		   return result;
+
+	
+
+}
+}
---
+++ src/main/java/io/hrms/fakeMernisService/FakeMernisValidationService.java
@@ -0,0 +1,15 @@
+package io.hrms.fakeMernisService;
+
+import io.hrms.entities.concretes.JobSeeker;
+
+public class FakeMernisValidationService {
+	
+	public boolean validatePerson(JobSeeker jobSeeker) {
+		
+		
+		
+		return true;
+	}
+	
+
+}
