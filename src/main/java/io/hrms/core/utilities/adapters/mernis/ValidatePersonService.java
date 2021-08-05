030064d86af39a9200a79ff4f58a6d4bea7c9406
merdemli <merveerdemli66@gmail.com>
2021-08-06 02:47:51 +0300

    Delete src/main/java/io/hrms/core/utilities/adapters/mernis directory

--- src/main/java/io/hrms/core/utilities/adapters/mernis/ValidatePersonService.java
+++
@@ -1,8 +0,0 @@
-package io.hrms.core.utilities.adapters.mernis;
-
-import io.hrms.entities.concretes.JobSeeker;
-
-public interface ValidatePersonService {
-	boolean CheckIfRealPerson(JobSeeker jobSeeker);
-
-}
