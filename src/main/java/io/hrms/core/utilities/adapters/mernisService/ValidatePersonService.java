e13d560d0a9824edc04b5222684b344857a32819
merdemli <57098477+merdemli@users.noreply.github.com>
2021-08-13 01:29:26 +0300

    Real Mernis Service Integrated (Adapter Design Pattern)

--- src/main/java/io/hrms/core/utilities/adapters/mernisService/ValidatePersonService.java
+++ src/main/java/io/hrms/core/utilities/adapters/mernisService/ValidatePersonService.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package io.hrms.core.utilities.adapters.mernisService;
 
 import java.rmi.RemoteException;
@@ -11,3 +12,18 @@ public interface ValidatePersonService {
 	boolean CheckIfRealPerson(JobSeeker jobSeeker) ;
 
 }
+=======
+package io.hrms.core.utilities.adapters.mernisService;
+
+import java.rmi.RemoteException;
+
+import org.springframework.stereotype.Service;
+
+import io.hrms.entities.concretes.JobSeeker;
+
+
+public interface ValidatePersonService {
+	boolean CheckIfRealPerson(JobSeeker jobSeeker) ;
+
+}
+>>>>>>> bab807a8d8f2c79f67d8904c704e2d00ccbb4b8c
